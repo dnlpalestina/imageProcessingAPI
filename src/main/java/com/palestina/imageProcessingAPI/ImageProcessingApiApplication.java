b9606861950852576d77c7578278073ecf9bc4b7
Daniel Palestina <dnlpalestina@gmail.com>
2023-01-17 18:24:08 -0600

    first commit

---
+++ src/main/java/com/palestina/imageProcessingAPI/ImageProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package com.palestina.imageProcessingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palestina/imageProcessingAPI/ImageProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palestina.imageProcessingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
